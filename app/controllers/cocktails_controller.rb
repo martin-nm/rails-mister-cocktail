5fbfa9837e05a1f9b53f3d1534ca02017a2c1a87
Martin Marinovic <mgnavarm@yahoo.com>
2019-11-15 16:52:59 +0100

    show cards ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class CocktailsController < ApplicationController
 
   def index
@@ -20,6 +22,14 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
+    parse = @cocktail.name.downcase
+    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{parse}"
+    cocktail_db_serialized = open(url).read
+    cocktail_db = JSON.parse(cocktail_db_serialized)
+    @img_url = cocktail_db['drinks'][0]['strDrinkThumb']
+    @category = cocktail_db['drinks'][0]['strCategory']
+    @iba = cocktail_db['drinks'][0]['strIBA']
+    @recipe = cocktail_db['drinks'][0]['strInstructions']
   end
 
   private
